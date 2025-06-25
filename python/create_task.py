d13f0ddf1e4ac694a095daff72414025e0c48802
PhilDev0815 <phildev0815@gmail.com>
2025-06-25 13:52:51 +0000

    chore(debug): Add hex logging to sender and receiver

--- python/create_task.py
+++ python/create_task.py
@@ -22,9 +22,15 @@ task.created_at.GetCurrentTime()
 try:
     # Serialisiere das Objekt in einen binären Byte-String
     serialized_task = task.SerializeToString()
-
+    print("--- Eindeutige DNA der Nachricht (ohne HEX) ---")
+    print(serialized_task)
+    print("----------------------------------------")
     # Kodiere den Byte-String in Base64 für den Versand via gcloud Pub/Sub
     base64_encoded_task = base64.b64encode(serialized_task).decode('utf-8')
+    # --- NEUE DIAGNOSE-AUSGABE ---
+    print("--- Eindeutige DNA der Nachricht (Hex) ---")
+    print(serialized_task.hex())
+    print("----------------------------------------")
 
     # Gib den finalen String aus
     print("--- Dein Base64-kodierter Task ---")
